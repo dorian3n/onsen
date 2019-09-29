5e9af6b8870f246d4fc82cc4aa2b520a53a28f7f
dorian3n <ti.ia11921929@gmail.com>
2019-09-30 07:31:46 +0900

    ビュー修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :hot_spring
 
-
+  # ユニークバリデーション
   validates_uniqueness_of :user, scope: :hot_spring
   validates :rate, presence: true
   validates :comment, presence: true
