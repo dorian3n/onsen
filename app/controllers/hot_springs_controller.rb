67ba468809be44950553557fafb7a50d5b9a463c
dorian3n <ti.ia11921929@gmail.com>
2019-09-30 10:03:03 +0900

    カミナリ

--- app/controllers/hot_springs_controller.rb
+++ app/controllers/hot_springs_controller.rb
@@ -34,7 +34,7 @@ class HotSpringsController < ApplicationController
   end
 
   def index
-    @hot_springs = HotSpring.all.order(created_at: :desc)
+    @hot_springs = HotSpring.page(params[:page]).per(5).order(created_at: :desc)
   end
 
   def edit
